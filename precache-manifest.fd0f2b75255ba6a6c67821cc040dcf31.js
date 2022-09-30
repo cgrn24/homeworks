5b3e851e9c58aaaafbe3b9ada26cd11ff1c5aa37
cgrn24 <stlhnt42@gmail.com>
2022-10-01 02:56:18 +0700

    Updates

---
+++ precache-manifest.fd0f2b75255ba6a6c67821cc040dcf31.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20af3a68ef210570259944b1df6b3aff",
+    "url": "/homeworks/index.html"
+  },
+  {
+    "revision": "3fd406a70cc1e0233f15",
+    "url": "/homeworks/static/css/main.a91ad8ea.chunk.css"
+  },
+  {
+    "revision": "5e971301eaa08debf81a",
+    "url": "/homeworks/static/js/2.a4f3a46c.chunk.js"
+  },
+  {
+    "revision": "915dc131845a888dd0ca37028e71cd51",
+    "url": "/homeworks/static/js/2.a4f3a46c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3fd406a70cc1e0233f15",
+    "url": "/homeworks/static/js/main.7348d505.chunk.js"
+  },
+  {
+    "revision": "dd098ccd315c55f9daed",
+    "url": "/homeworks/static/js/runtime-main.fb2d67e7.js"
+  }
+]);
