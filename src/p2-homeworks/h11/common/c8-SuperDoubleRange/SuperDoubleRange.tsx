314ebdb8e1e0503fd394c3b32e436f213c521188
cgrn24 <stlhnt42@gmail.com>
2022-10-30 03:09:00 +0700

    HW11

--- src/p2-homeworks/h11/common/c8-SuperDoubleRange/SuperDoubleRange.tsx
+++ src/p2-homeworks/h11/common/c8-SuperDoubleRange/SuperDoubleRange.tsx
@@ -1,24 +1,36 @@
-import React from 'react'
+import { Box, Slider } from '@mui/material'
+import React, { useEffect, useState } from 'react'
 
 type SuperDoubleRangePropsType = {
-    onChangeRange?: (value: [number, number]) => void
-    value?: [number, number]
-    // min, max, step, disable, ...
+  onChangeRange1: (value: number) => void
+  onChangeRange2: (value: number) => void
+  value?: any
+  // min, max, step, disable, ...
 }
 
-const SuperDoubleRange: React.FC<SuperDoubleRangePropsType> = (
-    {
-        onChangeRange, value,
-        // min, max, step, disable, ...
-    }
-) => {
-    // сделать самому, можно подключать библиотеки
+const SuperDoubleRange: React.FC<SuperDoubleRangePropsType> = ({
+  onChangeRange1,
+  onChangeRange2,
+  value,
+  // min, max, step, disable, ...
+}) => {
+  let [doubleValue, setDoubleValue] = React.useState<number[]>(value)
+  const handleChange = (event: Event, newValue: number | number[]) => {
+    setDoubleValue(newValue as number[])
+    // onChangeRange1(doubleValue[0])
+    // onChangeRange2(doubleValue[1])
+  }
+  useEffect(() => {
+    onChangeRange1(doubleValue[0])
+    onChangeRange2(doubleValue[1])
+  }, [doubleValue])
 
-    return (
-        <>
-            DoubleRange
-        </>
-    )
+  // сделать самому, можно подключать библиотеки
+  return (
+    <Box sx={{ width: 300 }}>
+      <Slider getAriaLabel={() => 'Temperature range'} value={value} onChange={handleChange} valueLabelDisplay='auto' />
+    </Box>
+  )
 }
 
 export default SuperDoubleRange
