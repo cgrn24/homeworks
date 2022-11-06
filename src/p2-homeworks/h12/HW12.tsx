3d1e6fcc71ee3588da4a0b6e6bd03092631ca32c
cgrn24 <stlhnt42@gmail.com>
2022-11-07 03:58:24 +0700

    HW12

--- src/p2-homeworks/h12/HW12.tsx
+++ src/p2-homeworks/h12/HW12.tsx
@@ -1,26 +1,32 @@
-import React from "react";
-import s from "./HW12.module.css";
+import React from 'react'
+import { useSelector } from 'react-redux'
+import { useDispatch } from 'react-redux'
+import { AppStoreType } from '../h10/bll/store'
+import SuperSelect from '../h7/common/c5-SuperSelect/SuperSelect'
+import SuperRadio from '../h7/common/c6-SuperRadio/SuperRadio'
+import { changeThemeC, ThemeType } from './bll/themeReducer'
+import s from './HW12.module.css'
 
-const themes = ['dark', 'red', 'some'];
+const themes = ['dark', 'red', 'some', 'pink', 'forest']
 
 function HW12() {
-    const theme = 'some'; // useSelector
-
-    // useDispatch, onChangeCallback
-
-    return (
-        <div className={s[theme]}>
-            <hr/>
-            <span className={s[theme + '-text']}>
-                homeworks 12
-            </span>
-
-            {/*should work (должно работать)*/}
-            {/*SuperSelect or SuperRadio*/}
-
-            <hr/>
-        </div>
-    );
+  const theme = useSelector<AppStoreType, string>((state) => state.themes.themes) // useSelector
+  const dispatch = useDispatch()
+  // useDispatch, onChangeCallback
+  const onChangeCallback = (callbackTheme: ThemeType) => {
+    dispatch(changeThemeC(callbackTheme))
+  }
+  return (
+    <div className={s[theme]}>
+      <hr />
+      <span className={s[theme + '-text']}>homeworks 12</span>
+      <br />
+      <SuperSelect value={theme} options={themes} onChangeOption={onChangeCallback} />
+      {/*should work (должно работать)*/}
+      {/*SuperSelect or SuperRadio*/}
+      <hr />
+    </div>
+  )
 }
 
-export default HW12;
+export default HW12
--- src/p2-homeworks/h12/bll/themeReducer.ts
+++ src/p2-homeworks/h12/bll/themeReducer.ts
@@ -1,14 +1,36 @@
 const initState = {
+  themes: 'dark',
+}
 
-};
-
-export const themeReducer = (state = initState, action: any): any => { // fix any
-    switch (action.type) {
-        case "": {
-            return state;
-        }
-        default: return state;
+export const themeReducer = (state = initState, action: changeThemeCType): any => {
+  // fix any
+  switch (action.type) {
+    case 'some': {
+      return { themes: action.type }
     }
-};
+    case 'dark': {
+      return { themes: action.type }
+    }
+    case 'red': {
+      return { themes: action.type }
+    }
+    case 'pink': {
+      return { themes: action.type }
+    }
+    case 'forest': {
+      return { themes: action.type }
+    }
+    default:
+      return state
+  }
+}
 
-export const changeThemeC = (): any => {}; // fix any
+export type ThemeType = 'some' | 'dark' | 'red' | 'pink' | 'forest'
+
+type changeThemeCType = {
+  type: ThemeType
+}
+
+export const changeThemeC = (theme: ThemeType): changeThemeCType => {
+  return { type: theme }
+} // fix any
